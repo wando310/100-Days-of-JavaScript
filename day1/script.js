d3e1d15d2eb1da0dd346a0090b54054ea5d4e824
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-07 11:13:51 -0300

    Add files via upload

---
+++ day1/script.js
@@ -0,0 +1,54 @@
+// Variable types with primitive types
+var name = 'Ana' //Strring-type var
+var age = 19 //integer-Type var
+var weight = 60.5 //Float-Type var
+var alive = true //Boolean-Type var
+var food = null //Null-Type var
+var time  //undefined-type var
+
+
+// Composite Variables -> Vector
+var fruits = ['Mango', 'Banana', 'BlueBerry', 'Pine Cone'] // Array
+
+
+//Object
+var person = {
+    name: 'Hannah', // For definition of the property value
+    age: 19,
+    alive: true,
+    weight: 60.5,
+}
+
+
+console.log(name) // The prints the values of the variables
+
+console.log(fruits) /*The prints the values of the Array -> Console.log(nameofarray)*/
+
+console.log(fruits[3]) //Betwwen [] is the position of the elements*/
+
+console.log(person) // The prints the values of the Object
+
+console.log(person.alive) // Put the . to access an object's value
+
+// Assign a new value to a variable
+    name = true
+    console.log(name)
+
+/* Assign a new one from a variable coming from another variable.
+
+    to take advantage of a variable to place its value on another property:
+
+    property: variable name
+
+    and print
+    console.log(property name and the used variable)
+*/
+
+// Know type of variable 
+
+console.log(typeof age)
+
+
+
+
+
