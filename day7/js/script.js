7e2ba0050ff9a73616141b294d1e11befa4819b1
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-13 10:28:21 -0300

    Prompt method

---
+++ day7/js/script.js
@@ -0,0 +1,8 @@
+// Ask the user for enter your name
+window.alert('Digite o seu nome a seguir para ser impresso no console')
+
+// Method for get value the user entered
+var value = prompt("Digite seu nome");
+
+//print the value
+console.log("Olá! " +  value + " prazer em conhece-lo(a) :)")
