8a29a63f1fe9caa53d09c676746d2dec1cfb83df
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-16 11:32:56 -0300

    Repeat Structure

---
+++ day10/js/script.js
@@ -0,0 +1,19 @@
+// Repeat Structure - For / While
+/* 
+    Using FOR  when your know its end
+
+    Using WHILE when your don't know when it's over
+
+*/
+
+for (var i = 0; i <=10; i++) {
+    console.log(i)
+}
+
+/* Uncomment to see code working
+
+while (i <=10) {
+    console.log(i)
+    i++
+    
+}*/
