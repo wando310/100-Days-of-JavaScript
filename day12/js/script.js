a3252f5d1e94e4e79e05e1c6313ccfb776ed2bfe
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-18 10:03:14 -0300

    Scope Block

---
+++ day12/js/script.js
@@ -0,0 +1,9 @@
+// Scope block 
+
+var car = '' /* Variable definition in global scope which will later receive the value of the variable through the function and print it correctly*/
+
+if (ture) {
+    var car = 'Tesla'
+}
+
+console.log(car)
