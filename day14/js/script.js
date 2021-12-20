e499329435c86f8d6cc160c239bd125a7ea81877
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-20 14:17:49 -0300

    Number Operators

---
+++ day14/js/script.js
@@ -0,0 +1,13 @@
+// Number Operators 
+
+var number = 10.3, number2 = 0, number3 = 4, number4 = 10
+
+var newValue = number.toString()
+var newValue2 = Math.trunc(number2)
+var newValue3= Math.ceil(number3)
+var newValue4 = Math.round(number4)
+
+console.log(newValue)
+console.log(newValue2)
+console.log(newValue3)
+console.log(newValue4)
