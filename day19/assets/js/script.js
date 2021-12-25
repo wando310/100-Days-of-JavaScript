26acac49f7c4024be3fe499be4b1da10edb50ed6
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-25 11:45:35 -0300

    Element Selection

---
+++ day19/assets/js/script.js
@@ -0,0 +1,18 @@
+var input = document.querySelector('div#app input.age')
+
+console.log(input)
+
+
+// What's I've learned
+document.getElementById // Select by ID
+document.getElementsByTagName // Select by TAG
+document.getElementsByClassName // Select by CLASS
+document.querySelector() // Select by specific selector 
+document.querySelectorAll // Select all by specific selector 
+
+.parentElement // Father 
+.parentElement.parentElement // Father's father
+.previousElementSibling // Previus Element 
+.nextElementSibling // Next Element 
+.children // HTMLCollection with the sons
+.children[0] // First son 
