beefde64a90497d2ed858c929de47f868f7d4225
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-28 14:39:57 -0300

    Events Manipulation

---
+++ day20/script.js
@@ -0,0 +1,5 @@
+var button = document.querySelector('#app button')
+
+button.addEventListener('click', function () {
+    alert('Sua idade é: ' + input.value + 'foi armazenada em nosso banco de dados')
+})
