0cbf8556d23ad15057272617cc9bd7dfa33bb555
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-12-29 11:16:06 -0300

    Changing Content

---
+++ day23/app.js
@@ -0,0 +1,7 @@
+var app = document.querySelector('div#app')
+
+// .outerHTML - Todo o HTML do elemento
+// .innerHTML - HTML Interno
+// .innerText - todo o texto, sem tags 
+
+app.innerHTML = '<p>Testando alterações de conteúdo em JavaScript</p>'
