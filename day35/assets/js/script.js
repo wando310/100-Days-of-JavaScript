b91382d4637f8a86c2da8be7c7280729c91b20c3
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2022-01-17 13:39:22 -0300

    External JS

---
+++ day35/assets/js/script.js
@@ -0,0 +1,17 @@
+let msg = document.getElementById("exit") 
+
+function action1 () {
+    msg.innerHTML = 'Você clicou no <span>PRIMEIRO</span> botão'
+}
+
+function action2 () {
+    msg.innerHTML = 'Você clicou no <span>SEGUNDO</span> botão'
+}
+
+function action3 () {
+    msg.innerHTML = 'Você clicou no <span>TERCEIRO</span> botão'
+}
+
+function action4 () {
+    msg.innerHTML = 'Você clicou no <span>QUARTO</span> botão'
+}
